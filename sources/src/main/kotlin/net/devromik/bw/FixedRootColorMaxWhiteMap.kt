d2556523f1b2e974fb8e16693b9a98daff54621f
devromik <dev.romik@gmail.com>
2016-06-26 20:30:01 +0300

    Add a factory method allowing for making FixedRootColorMaxWhite for a single node

--- sources/src/main/kotlin/net/devromik/bw/FixedRootColorMaxWhiteMap.kt
+++ sources/src/main/kotlin/net/devromik/bw/FixedRootColorMaxWhiteMap.kt
@@ -8,24 +8,42 @@ import java.lang.Math.max
  */
 open class FixedRootColorMaxWhiteMap(override val treeSize: Int) : MaxWhiteMap {
 
-    val maps = Array(COLOR_COUNT, { WritableMaxWhiteMap(treeSize) })
+    private val maps = Array(COLOR_COUNT, { WritableMaxWhiteMap(treeSize) })
+
+    private fun mapFor(rootColor: Color) = maps[rootColor.index]
+
+    // ****************************** //
+
+    companion object {
+
+        fun forSingleNode(): FixedRootColorMaxWhiteMap {
+            val singleNodeMaxWhiteMap = FixedRootColorMaxWhiteMap(treeSize = 1)
+
+            singleNodeMaxWhiteMap.set(rootColor = BLACK, black = 0, maxWhite = INVALID_MAX_WHITE)
+            singleNodeMaxWhiteMap.set(rootColor = BLACK, black = 1, maxWhite = 0)
+
+            singleNodeMaxWhiteMap.set(rootColor = WHITE, black = 0, maxWhite = 1)
+            singleNodeMaxWhiteMap.set(rootColor = WHITE, black = 1, maxWhite = INVALID_MAX_WHITE)
+
+            singleNodeMaxWhiteMap.set(rootColor = GRAY, black = 0, maxWhite = 0)
+            singleNodeMaxWhiteMap.set(rootColor = GRAY, black = 1, maxWhite = INVALID_MAX_WHITE)
+
+            return singleNodeMaxWhiteMap
+        }
+    }
 
     // ****************************** //
 
     operator fun set(rootColor: Color, black: Int, maxWhite: Int) {
-        maxWhiteFor(rootColor)[black] = maxWhite
+        mapFor(rootColor)[black] = maxWhite
     }
 
     open operator fun get(rootColor: Color, black: Int): Int {
-        return maxWhiteFor(rootColor)[black]
+        return mapFor(rootColor)[black]
     }
 
     override operator fun get(black: Int): Int {
         val blackWhiteMax = max(get(BLACK, black), get(WHITE, black))
         return max(blackWhiteMax, get(GRAY, black))
     }
-
-    // ****************************** //
-
-    private fun maxWhiteFor(rootColor: Color) = maps[rootColor.index]
 }
---
+++ sources/src/test/kotlin/net/devromik/bw/FixedRootColorMaxWhiteMapTest.kt
@@ -0,0 +1,15 @@
+package net.devromik.bw
+
+import org.junit.Test
+import org.junit.Assert.*
+
+/**
+ * @author Shulnyaev Roman
+ */
+class FixedRootColorMaxWhiteMapTest {
+
+    @Test
+    fun testSet() {
+
+    }
+}
