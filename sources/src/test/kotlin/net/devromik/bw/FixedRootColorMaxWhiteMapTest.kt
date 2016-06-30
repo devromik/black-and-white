4ca969abf8e55823835f01ca2fdcba50e69c4254
devromik <dev.romik@gmail.com>
2016-06-30 17:37:20 +0300

    Add test FixedRootColorMaxWhiteMap.canBuildMapForSingleNode()

--- sources/src/test/kotlin/net/devromik/bw/FixedRootColorMaxWhiteMapTest.kt
+++ sources/src/test/kotlin/net/devromik/bw/FixedRootColorMaxWhiteMapTest.kt
@@ -1,5 +1,6 @@
 package net.devromik.bw
 
+import net.devromik.bw.Color.*
 import org.junit.Test
 import org.junit.Assert.*
 
@@ -8,8 +9,25 @@ import org.junit.Assert.*
  */
 class FixedRootColorMaxWhiteMapTest {
 
-    @Test
-    fun testSet() {
+    @Test fun canBuildMapForSingleNode() {
+        val maxWhiteMap = FixedRootColorMaxWhiteMap.forSingleNode()
 
+        // BLACK
+        assertEquals(INVALID_MAX_WHITE, maxWhiteMap.get(rootColor = BLACK, black = -1))
+        assertEquals(INVALID_MAX_WHITE, maxWhiteMap.get(rootColor = BLACK, black = 0))
+        assertEquals(0,                 maxWhiteMap.get(rootColor = BLACK, black = 1))
+        assertEquals(INVALID_MAX_WHITE, maxWhiteMap.get(rootColor = BLACK, black = 2))
+
+        // WHITE
+        assertEquals(INVALID_MAX_WHITE, maxWhiteMap.get(rootColor = WHITE, black = -1))
+        assertEquals(1,                 maxWhiteMap.get(rootColor = WHITE, black = 0))
+        assertEquals(INVALID_MAX_WHITE, maxWhiteMap.get(rootColor = WHITE, black = 1))
+        assertEquals(INVALID_MAX_WHITE, maxWhiteMap.get(rootColor = WHITE, black = 2))
+
+        // GRAY
+        assertEquals(INVALID_MAX_WHITE, maxWhiteMap.get(rootColor = GRAY, black = -1))
+        assertEquals(0,                 maxWhiteMap.get(rootColor = GRAY, black = 0))
+        assertEquals(INVALID_MAX_WHITE, maxWhiteMap.get(rootColor = GRAY, black = 1))
+        assertEquals(INVALID_MAX_WHITE, maxWhiteMap.get(rootColor = GRAY, black = 2))
     }
 }
