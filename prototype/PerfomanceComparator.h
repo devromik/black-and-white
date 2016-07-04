cf7be8c1c900278936e8c0e234f0bc32b2df37b6
devromik <dev.romik@gmail.com>
2016-07-04 16:38:28 +0300

    Remove the prototype

--- prototype/PerfomanceComparator.h
+++
@@ -1,8 +0,0 @@
-#ifndef BW_PERFOMANCE_COMPARATOR_H
-#define BW_PERFOMANCE_COMPARATOR_H
-
-namespace bw {
-    //
-} // namespace bw.
-
-#endif
--- prototype/PerfomanceComparisonView.h
+++
@@ -1,8 +0,0 @@
-#ifndef BW_PERFOMANCE_COMPARISON_VIEW_H
-#define BW_PERFOMANCE_COMPARISON_VIEW_H
-
-namespace bw {
-    //
-} // namespace bw.
-
-#endif
